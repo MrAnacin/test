01f4a6de9b062b0b282e186a1327619bfbbdc8ed
Dmitry <jagotti@yandex.ru>
2023-07-20 09:49:34 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import AppRoutes from "./routes";
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import RegisterPage from './components/registartion/reg';
-import LoginPage from './components/registartion/login';
-import FireBase from "./firebase";
+// import FireBase from "./firebase";
 
 import style from "./app.module.scss"
 
 function App() {
   return (
     <div className={style.app}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/reg" element={<RegisterPage />} />
-      </Routes>
-    </Router>
+      <AppRoutes/>
     </div>
   );
 }
