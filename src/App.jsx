38110425e6c9a561215b35c9157a14bf053aa8d1
Dmitry <jagotti@yandex.ru>
2023-07-25 23:17:26 +0300

    add firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import AppRoutes from "./routes";
 import React from 'react';
-
 import style from "./app.module.scss"
+import database from "./firebase";
 
 function App() {
+ 
+  console.log(database);
+  
   return (
     <div className={style.app}>
       <AppRoutes/>
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,22 @@
-// import { initializeApp } from "firebase/app";
+// import  firebase from "firebase";
 // import RegisterPage from './components/registartion/reg';
 // import LoginPage from './components/registartion/login';
+import { initializeApp } from "firebase/app"
+import { getDatabase } from "firebase/database";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB-QzO4rIQgzEashkMMdaZKaBbrvSg3Voc",
-//   authDomain: "fitness-pro-team5.firebaseapp.com",
-//   databaseURL: "https://fitness-pro-team5-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "fitness-pro-team5",
-//   storageBucket: "fitness-pro-team5.appspot.com",
-//   messagingSenderId: "35316317842",
-//   appId: "1:35316317842:web:5e369ceb8252e73e5f304b"
-// };
 
-// const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyB-QzO4rIQgzEashkMMdaZKaBbrvSg3Voc",
+  authDomain: "fitness-pro-team5.firebaseapp.com",
+  databaseURL: "https://fitness-pro-team5-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fitness-pro-team5",
+  storageBucket: "fitness-pro-team5.appspot.com",
+  messagingSenderId: "35316317842",
+  appId: "1:35316317842:web:5e369ceb8252e73e5f304b"
+};
 
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+export default database;
